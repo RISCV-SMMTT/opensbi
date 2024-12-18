30e2f1680e35255e371a7ac441bc3f249d1ae165
FXD <52352730+Xisaname@users.noreply.github.com>
2024-12-18 10:46:31 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-export ARCH=riscv
-export CROSS_COMPILE=riscv64-linux-gnu-
-export CC="${CROSS_COMPILE}gcc -mabi=lp64d -march=rv64gc"
-make PLATFORM=generic PLATFORM_RISCV_XLEN=64 DEBUG=1
-rm -rf /home/xiao/Documents/kvm/smmtt/qemu/pc-bios/opensbi-riscv64-generic-fw_dynamic.bin
-cp ./build/platform/generic/firmware/fw_dynamic.bin \
- ../smmtt/qemu/pc-bios/opensbi-riscv64-generic-fw_dynamic.bin
