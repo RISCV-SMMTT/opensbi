801703cae7a7f33df4782d7ca3ee1910cff9f75b
Xiao <1335052896@qq.com>
2025-07-29 23:52:26 -0700

    bug: fix the name error.

--- include/sbi/riscv_encoding.h
+++ include/sbi/riscv_encoding.h
@@ -444,7 +444,7 @@
 #define CSR_VSIREG6         		0x257
 
 /* Supervisor Domain Identifier Extension */
-#define CSR_MTTP				0x3e0
+#define CSR_MMPT				0x3e0
 
 /* VS-Level Interrupts (H-extension with AIA) */
 #define CSR_VSTOPEI			0x25c
